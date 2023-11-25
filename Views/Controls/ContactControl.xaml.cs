8108c4bc1a9dc5223bd1a0b23521da1daafe7f75
Ariel Tyson <ariel30190@hotmail.com>
2023-11-25 06:38:57 -0800

    Refactor EditContactPage to use reusable control

---
+++ Views/Controls/ContactControl.xaml.cs
@@ -0,0 +1,71 @@
+﻿namespace contacts_app.Views.Controls;
+
+public partial class ContactControl : ContentView
+{
+
+    public event EventHandler<string> OnSave;
+    public event EventHandler<EventArgs> OnCancel;
+
+	public ContactControl()
+	{
+		InitializeComponent();
+	}
+
+	public string Name
+	{
+		get
+		{
+			return entryName.Text;
+		}
+		set
+		{
+			entryName.Text = value;
+		}
+	}
+
+    public string Email
+    {
+        get
+        {
+            return entryEmail.Text;
+        }
+        set
+        {
+            entryEmail.Text = value;
+        }
+    }
+
+    public string Address
+    {
+        get
+        {
+            return entryAddress.Text;
+        }
+        set
+        {
+            entryAddress.Text = value;
+        }
+    }
+
+    public string Phone
+    {
+        get
+        {
+            return entryPhone.Text;
+        }
+        set
+        {
+            entryPhone.Text = value;
+        }
+    }
+
+    void cancelButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        OnCancel?.Invoke(sender, e);
+    }
+
+    void saveButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        OnSave?.Invoke(sender, e.ToString());
+    }
+}
